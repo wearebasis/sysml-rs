5e07787e628502f577066fd7b68feda97397476e
RickyMillar <145624780+RickyMillar@users.noreply.github.com>
2026-01-26 13:13:21 +1300

    Add tree-sitter highlight queries

---
+++ sysml-ts/tree-sitter/queries/highlights.scm
@@ -0,0 +1,33 @@
+[
+  "package"
+  "import"
+  "part"
+  "attribute"
+  "action"
+  "state"
+  "interface"
+  "port"
+  "requirement"
+  "constraint"
+  "enum"
+  "type"
+  "def"
+] @keyword
+
+(comment) @comment
+
+["true" "false" "null"] @constant.builtin
+
+(package_decl name: (identifier) @module)
+(import_decl path: (import_path) @string.special)
+
+(part_def name: (identifier) @type)
+(attribute_def name: (identifier) @type)
+(definition name: (identifier) @type)
+
+(part_usage name: (identifier) @variable)
+(attribute_usage name: (identifier) @property)
+(usage name: (identifier) @variable)
+
+(type_ref (identifier) @type)
+(qualified_name (identifier) @type)
